64328793ae5f47fe08de789262c4460a2a2b4967
san <sanjose0023@gmail.com>
2025-10-07 21:00:06 +0530

    feat: add merchant Dashboard

---
+++ src/app/components/MerchantDashboardView/MerchantDashboardView.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MerchantDashboardView = () => {
+    return (
+        <div>
+            Merchant dashboard
+        </div>
+    )
+}
+
+export default MerchantDashboardView
