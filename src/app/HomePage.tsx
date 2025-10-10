b2d02dd4c0225edd65e38bd3d4e2dce0881b9efd
san <sanjose0023@gmail.com>
2025-10-10 11:49:49 +0530

    refactor: home page

---
+++ src/app/HomePage.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import React from 'react';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Banner from './components/Home/Banner/Banner';
+import Advantages from './components/Home/Advantages/Advantages';
+import Category from './components/Home/Category/Category';
+import Products from './components/Home/Products/Products';
+import WeekendOffers from './components/Home/WeekendOffers/WeekendOffers';
+import TopSelling from './components/Home/TopSelling/TopSelling';
+import OrderBanner from './components/Home/OrderBanner/OrderBanner';
+import TopVendors from './components/Home/TopVendors/TopVendors';
+import SpecialOffer from './components/Home/SpecialOffer/SpecialOffer';
+import WeeklyTopVenders from './components/Home/WeeklyTopVenders/WeeklyTopVenders';
+import CustomerServices from './components/Home/CustomerServices/CustomerServices';
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Advantages />
+      <Category />
+      <Products />
+      <WeekendOffers />
+      <TopSelling />
+      <OrderBanner />
+      <TopVendors />
+      <SpecialOffer />
+      <CustomerServices />
+      <WeeklyTopVenders />
+      <Footer />
+    </>
+  );
+};
+
+export default HomePage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,21 @@
-import React from 'react'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Footer from './components/Footer/Footer'
-import Header from './components/Header/Header'
-import Banner from './components/Home/Banner/Banner'
-import Advantages from './components/Home/Advantages/Advantages'
-import Category from './components/Home/Category/Category'
-import Products from './components/Home/Products/Products'
-import WeekendOffers from './components/Home/WeekendOffers/WeekendOffers'
-import TopSelling from './components/Home/TopSelling/TopSelling'
-import OrderBanner from './components/Home/OrderBanner/OrderBanner'
-import TopVendors from './components/Home/TopVendors/TopVendors'
-import SpecialOffer from './components/Home/SpecialOffer/SpecialOffer'
-// import CustomerServices from './components/CustomerServices/CustomerServices'
-import WeeklyTopVenders from './components/Home/WeeklyTopVenders/WeeklyTopVenders';
-import CustomerServices from './components/Home/CustomerServices/CustomerServices';
+// import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
+import HomePage from './HomePage';
+import { auth } from '../../auth';
 
-const page = () => {
-  return (
-    <>
-      <Header />
-      <Banner/>
-      <Advantages/>
-      <Category/>
-      <Products/>
-      <WeekendOffers/>
-      <TopSelling/>
-      <OrderBanner/>
-      <TopVendors/>
-      <SpecialOffer/>
-      <CustomerServices/>
-      <WeeklyTopVenders/>
-      <Footer />
-    </>
-  )
+export default async function Page() {
+  const session = await auth();
+
+  // If user is not logged in, redirect to login
+  if (!session?.user) {
+    redirect('/auth/login');
+  }
+
+  // If user is a Merchant, redirect to dashboard
+  if (session.user.role === 'Merchant') {
+    redirect('/dashboard');
+  }
+
+  // Normal users can access home page
+  return <HomePage />;
 }
-
-export default page
