1d2f6950d4ca6481da9fdcdb7ebd78f723f0d68f
sanvarghese <sanjose0023@gmail.com>
2026-05-14 17:20:12 +0530

    add footer and header in current cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Cart from '../components/Cart/Cart'
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
 
 const page = () => {
   return (
-    <Cart/>
+    <>
+      <Header />
+      <Cart />
+      <Footer />
+    </>
   )
 }
 
