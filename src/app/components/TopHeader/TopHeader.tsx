8ab0834bad901d04290b736c9b12e323686f02ac
sanvarghese <sanjose0023@gmail.com>
2026-05-30 12:51:10 +0530

    refactor: orders navigation in topHeader

--- src/app/components/TopHeader/TopHeader.tsx
+++ src/app/components/TopHeader/TopHeader.tsx
@@ -111,13 +111,23 @@ const TopHeader = () => {
 
                                         <ul className={`dropdown-menu dropdown-menu-end ${isDropdownOpen ? 'show' : ''}`}
                                             style={{ position: 'absolute', right: 0, top: '100%', marginTop: '0.5rem' }}>
-
                                             <li>
                                                 <Link className="dropdown-item" href="/my-account" onClick={() => setIsDropdownOpen(false)}>
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person me-2" viewBox="0 0 16 16">
                                                         <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
                                                     </svg>
                                                     My Profile
+                                                </Link>
+                                            </li>
+                                            <li>
+                                                <Link className="dropdown-item" href="/my-account">
+                                                    {/* <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person me-2" viewBox="0 0 16 16">
+                                                        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+                                                    </svg> */}
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-seam me-2" viewBox="0 0 16 16">
+                                                        <path d="M9.828.122a1 1 0 0 0-.656 0l-8 3A1 1 0 0 0 .5 4.06V12a1 1 0 0 0 .672.938l8 3a1 1 0 0 0 .656 0l8-3A1 1 0 0 0 15.5 12V4.06a1 1 0 0 0-.672-.938zM8 1.21 14.5 3.65 8 6.09 1.5 3.65zM1.5 4.74 7.5 7v7.09l-6-2.25zm7 9.35V7l6-2.26v7.1z" />
+                                                    </svg>
+                                                    Orders
                                                 </Link>
                                             </li>
 
